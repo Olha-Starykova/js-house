bde76cf20b18d09746e465a6b14e0b0cda443f68
Olha-Starykova <69404676+Olha-Starykova@users.noreply.github.com>
2020-10-22 01:30:48 +0300

    на второй части

---
+++ js/01-vars.js
@@ -0,0 +1,37 @@
+// 'use strict';  строгий режим. пишем если не в модуле
+// 1
+// let hobby = "Рыбалка";
+// /меняем имя переменной/ 
+// hobby = "Шашки";
+// /выводим на консоль/  
+// console.log(hobby);
+// /нельзя использовать переменую до ее объявления!/ 
+
+// // 2
+// //строка 
+// const age = 10;
+// // литерал числа
+// console.log(age);
+// // буль да или нет. Не существительные.
+// const isVIP = true;
+// const firstName = 'Chelsy'
+// console.log(firstName);
+// console.log(isVIP);
+// // как узнать, что там лежит typeof тип значения
+// const type = typeof 5;
+// console.log(type);
+// const typ = typeof 'Mango';
+// console.log(typ);
+// const ty = typeof true;
+// console.log(ty);
+
+// console.log(typeof firstName);
+
+// 3
+// вывод на консоль
+// console.log('This is a number: ', 10);
+// //модалка)))
+// alert('Hello');
+// console.log('after');
+
+
---
+++ js/02-input.js
@@ -0,0 +1,48 @@
+/*
+ * Ввод пользовователя
+ */
+// 1
+// confirm('Хотите продлить подписку?');
+// //  браузер спрашивает ок или отмена..висит модалка
+// // оборачиваем в переменную....продлить подписку или нет..
+// const shouldRenew = confirm('Хотите продлить подписку?');
+// // выводим ответ в консоль, это буль
+// console.log(shouldRenew);
+
+//2.1
+// // получаем значение от пользователя
+// prompt('Введите число 10');
+// // записываем в переменную.
+// const userInput = prompt('Введите число 10');
+// console.log(userInput);
+// null отмена, строчные возращают строчные. число 10 это строка.
+// console.log(typeof userInput);
+// console.log(userInput === 10);
+// буль говорит, что folse
+// 2.2
+// перезапишим в др переменную
+// let userInput = prompt('Введите число 10');
+// // преобразуем в число Namber
+// userInput = Number(userInput);
+// console.log(typeof userInput);
+// console.log(userInput === 10);
+// // пишет true
+
+// 3 Сравнение
+
+
+
+// const shouldRenew = confirm('Хотите продлить подписку?');
+// const userInput = prompt('Введите ваше имя');
+
+// console.log(userInput);
+
+// const userInput = prompt('Введите число 5!');
+
+// console.log(userInput);
+// console.log(typeof userInput);
+
+// const number = Number(userInput);
+// console.log(number);
+// console.log(typeof number);
+// console.log(number === 5);
