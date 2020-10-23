09339e04d60a0bce302db6741861babe1cd370d1
Olha-Starykova <69404676+Olha-Starykova@users.noreply.github.com>
2020-10-23 04:07:37 +0300

    ком

--- js/01-vars.js
+++ js/01-vars.js
@@ -34,4 +34,62 @@
 // alert('Hello');
 // console.log('after');
 
+//*** 0-3 */
+//  const userPassword = 'jqueryismyjam';
+
+// const ADMIN_PASSWORD = 'jqueryismyjam';
+
+// const CANCELED_BY_USER = 'Отменено пользователем!';
+// const ACCESS_IS_ALLOWED = 'Добро пожаловать!';
+// const ACCESS_DENIED = 'Доступ запрещен, неверный пароль!';
+// let message;
+
+// // Write code under this line
+// if (userPassword === ADMIN_PASSWORD) {
+//     message = ACCESS_IS_ALLOWED;
+// } else if (userPassword === null) {
+//     message = CANCELED_BY_USER;
+// } else if (userPassword === 'bla') {
+//     message = ACCESS_DENIED;
+// }
+// console.log(message);
+
+//***0-4 */
+
+// const orderPieces = 4;
+
+// const credits = 23580;
+// const pricePerDroid = 3000;
+// const CANCELED_BY_USER = 'Отменено пользователем!';
+// const ACCESS_DENIED = 'Недостаточно средств на счету!';
+
+// let totalPrice; // Write code on this line
+// let balanceCredit;  // Write code on this line
+// let message;
+
+
+// // Write code under this line
+// if (orderPieces === null) {
+//     message = CANCELED_BY_USER;
+// } else if (orderPieces <= 7) {
+//     totalPrice = pricePerDroid * orderPieces;
+//     balanceCredit = credits - totalPrice;
+//     message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceCredit} кредитов`
+// } else if (orderPieces <= 12) {
+//     message = ACCESS_DENIED;
+// }
+// console.log(message);
+// Write code under this line
+
+//если orderPieces равно 4
+// то значение message будет равно
+// 'Вы купили 4 дроидов, на счету осталось 11580 кредитов'
+
+//если orderPieces равно null
+// то значение message будет равно
+// 'Отменено пользователем!'
+
+//если orderPieces равно 10
+// то значение message будет равно
+// 'Недостаточно средств на счету!'
 
